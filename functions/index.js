fd9348a3abf4ce5e4541156b21dc865fde541788
muhammad <m.karim0024@gmail.com>
2025-07-19 14:22:04 +0100

    added search patient function searchPatientsByNameAndDOB

--- functions/index.js
+++ functions/index.js
@@ -39,6 +39,37 @@ exports.listPatients = functions.https.onRequest((_req, res) => {
   ])
 })
 
+
+
+exports.searchPatientsByNameAndDOB = functions.https.onRequest(async (req, res) => {
+  const name = req.query.name
+  const dob = req.query.dob
+
+  if (!name || !dob) {
+    return res.status(400).send('Missing "name" or "dob" query parameters')
+  }
+
+  try {
+    const snapshot = await admin.firestore().collection('patients')
+      .where('name', '==', name)
+      .where('dob', '==', dob)
+      .get()
+
+    if (snapshot.empty) {
+      return res.status(404).send('No matching patients found')
+    }
+
+    const patients = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    res.status(200).json(patients)
+  } catch (error) {
+    console.error("Error querying patients:", error)
+    res.status(500).send(error.message)
+  }
+})
+
+
+
+
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
 // traffic spikes by instead downgrading performance. This limit is a
