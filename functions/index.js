e1095237db818696128efd9515a60e0bf361532a
tahmidachoudhury <tahmidachoudhury@outlook.com>
2025-07-19 14:11:55 +0100

    push functions

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,37 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const { setGlobalOptions } = require("firebase-functions")
+const { onRequest } = require("firebase-functions/https")
+const logger = require("firebase-functions/logger")
+
+// functions/index.js
+const functions = require("firebase-functions")
+
+/**
+ * Dummy endpoint:
+ * GET https://<region>-<project-id>.cloudfunctions.net/getPatient
+ * Always returns the same mock patient record.
+ */
+exports.getPatient = functions.https.onRequest((_req, res) => {
+  res.json({
+    id: "123",
+    name: "Ahmed",
+    allergy: "Penicillin",
+  })
+})
+
+/**
+ * Another example endpoint:
+ * GET https://<region>-<project-id>.cloudfunctions.net/listPatients
+ * Returns an array of two hard-coded patients.
+ */
+exports.listPatients = functions.https.onRequest((_req, res) => {
+  res.json([
+    { id: "123", name: "Ahmed", allergy: "Penicillin" },
+    { id: "456", name: "Fatima", allergy: "None" },
+  ])
+})
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -21,7 +49,7 @@ const logger = require("firebase-functions/logger");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({ maxInstances: 10 })
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
